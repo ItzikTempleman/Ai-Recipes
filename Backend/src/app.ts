e1dcfffcad402dd67481c431dae231843a907967
itzik templeman <itzik.templeman@gmail.com>
2025-10-01 22:03:29 +0300

    d

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,11 +1,11 @@
 import cors from "cors";
 import express from "express";
-import mongoose from "mongoose";
 import { appConfig } from "./2-utils/app-config";
 import { recipeController } from "./5-controllers/recipe-controller";
 import { errorMiddleware } from "./6-middleware/error-middleware";
 import path from "path";
 import { fileSaver } from "uploaded-file-saver";
+import fileUpload from "express-fileupload";
 
 class App {
     public async start(): Promise<void> {
@@ -13,24 +13,21 @@ class App {
         server.use(cors());
         server.use(express.json());
 
-            server.use(fileUpload());
+        server.use(fileUpload());
         const location = path.join(__dirname, "1-assets", "images");
         fileSaver.config(location);
 
-        
         server.use(recipeController.router);
         server.use(errorMiddleware.routeNotFound);
         server.use(errorMiddleware.catchAll);
+
         server.listen(appConfig.port, () => {
             console.log(`Listening on http://${appConfig.host}:${appConfig.port}`);
-          }
+        }
         );
     }
 }
 
 const app = new App();
 app.start();
-function fileUpload(): any {
-    throw new Error("Function not implemented.");
-}
 
