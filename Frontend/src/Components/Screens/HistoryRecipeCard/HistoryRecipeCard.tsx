a901efbd08a83d9b89966d138464ab5b2114c30d
itzik templeman <itzik.templeman@gmail.com>
2025-10-14 09:57:01 +0300

    test: small change to verify branch safety

--- Frontend/src/Components/Screens/HistoryRecipeCard/HistoryRecipeCard.tsx
+++ Frontend/src/Components/Screens/HistoryRecipeCard/HistoryRecipeCard.tsx
@@ -9,11 +9,12 @@ export function HistoryRecipeCard({ recipe }: RecipeProps) {
     const cardClassName = recipe.imageUrl 
         ? "HistoryRecipeCard" 
         : "NoImageHistoryRecipeCard";
-
+console.log("🌱 Test change — feature branch only");
     return (
         <div className={cardClassName}>
             {recipe.imageUrl && (
                 <img className="HistoryCardImage" src={recipe.imageUrl} />
+                
             )}
             <div className="HistoryCardTitleContainer">
             <div className="HistoryCardTitle">{recipe.title.title}</div>
