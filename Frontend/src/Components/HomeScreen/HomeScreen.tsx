732df1613822e258b3009c0ab52fe1cb0bc5f7ec
itzik templeman <itzik.templeman@gmail.com>
2025-12-26 12:02:53 +0200

    d

--- Frontend/src/Components/HomeScreen/HomeScreen.tsx
+++ Frontend/src/Components/HomeScreen/HomeScreen.tsx
@@ -2,27 +2,28 @@ import { useEffect } from "react";
 import LibraryAdd from "@mui/icons-material/LibraryAdd";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
-
+import LoginIcon from '@mui/icons-material/Login';
 import "./HomeScreen.css";
 import { useTitle } from "../../Utils/Utils";
 import { AppState } from "../../Redux/Store";
 import { recipeService } from "../../Services/RecipeService";
 import { RecipeListItem } from "../RecipeListItem/RecipeListItem";
 import { useNavigate } from "react-router-dom";
+
 export function HomeScreen() {
   useTitle("Home");
   const { items } = useSelector((state: AppState) => state.recipes);
   const user = useSelector((state: AppState) => state.user);
   const { t, i18n } = useTranslation();
   const isRTL = (i18n.language ?? "").startsWith("he");
- const navigate = useNavigate();
+  const navigate = useNavigate();
 
 
   useEffect(() => {
-      const token = localStorage.getItem("token");
-  if (!token) return;
-    recipeService.getAllRecipes().catch(() => {});
-    recipeService.loadMyLikes().catch(() => {});
+    const token = localStorage.getItem("token");
+    if (!token) return;
+    recipeService.getAllRecipes().catch(() => { });
+    recipeService.loadMyLikes().catch(() => { });
   }, [user?.id]);
 
   const list = Array.isArray(items) ? items : [];
@@ -50,13 +51,16 @@ export function HomeScreen() {
           <div className="HomeScreenTitleContainer">
             <div className="GuestNotice">
               <div className="GuestHello">{t("homeScreen.hello")} {t("homeScreen.guest")}</div>
-              
-                <div className="GuestInstructions" 
-               onClick={()=>navigate("/generate")}
-               ><LibraryAdd/><h4 className="GuestGenerate">{t("homeScreen.generate")}</h4></div>
-              
+
+              <div className="GuestInstructions"
+                onClick={() => navigate("/generate")}
+              ><LibraryAdd /><h4 className="GuestGenerate">{t("homeScreen.generate")}</h4></div>
+
               <div className="GuestNoticeLine1">{t("homeScreen.guestNoticeLine1")}</div>
               <h3 className="GuestNoticeLine2">{t("homeScreen.guestNoticeLine2")}</h3>
+              <div className="GuestLoginContainer"
+                onClick={() => navigate("/login")}
+              ><LoginIcon /><h4 className="GuestLoginTitle">{t("drawer.login")}</h4></div>
             </div>
           </div>
         )}
