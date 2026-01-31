c66802e2600bacb2ec9e4c1a092a30ed102141b0
itzik templeman <itzik.templeman@gmail.com>
2026-01-31 17:40:02 +0200

    ddd

--- Frontend/src/Components/recipe-components/SuggestionsScreen/SuggestionsScreen.tsx
+++ Frontend/src/Components/recipe-components/SuggestionsScreen/SuggestionsScreen.tsx
@@ -1,24 +1,8 @@
-import { useSelector } from "react-redux";
 import { useTitle } from "../../../Utils/Utils";
 import "./SuggestionsScreen.css";
-import { AppState } from "../../../Redux/Store";
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 export function SuggestionsScreen() {
     useTitle("Suggested");
-    const { items } = useSelector((state: AppState) => state.recipes);
-    const user = useSelector((state: AppState) => state.user);
-    const { t, i18n } = useTranslation();
-    const isRTL = (i18n.language ?? "").startsWith("he");
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (!token) return;
-        //TODO
-    }, [user?.id]);
 
     return (
         <div className="SuggestionsScreen">
