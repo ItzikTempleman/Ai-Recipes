3884ba36d97d5091f61404ae7da39412351414c5
itzik templeman <itzik.templeman@gmail.com>
2026-03-14 19:25:34 +0200

    f

--- Frontend/src/Components/RecipeComponents/FeatureHint/FeatureHint.tsx
+++ Frontend/src/Components/RecipeComponents/FeatureHint/FeatureHint.tsx
@@ -1,12 +1,7 @@
 import { useSelector } from "react-redux";
 import "./FeatureHint.css";
-
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-
-import HistoryIcon from "@mui/icons-material/History";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import BubbleChartIcon from "@mui/icons-material/BubbleChart";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { AppState } from "../../../Redux/Store";
@@ -23,30 +18,30 @@ export function FeatureHint() {
   return (
     <div className={` ${user ? "user" : "guest"}  `}>
       {!user && (
-
+<div className="container">
         <div className="FeatureHint" dir={dir}>
        
 <div className="ChatSection FeatureRow">
-  <span className="FeaterIcon">
+  {/* <span className="FeaterIcon">
     <BubbleChartIcon />
-  </span>
+  </span> */}
   <span className="FeatureLabel">{t("homeScreen.ask")}</span>
 </div>
 
 <div className="FavoritesSection FeatureRow">
-  <span className="FeaterIcon">
+  {/* <span className="FeaterIcon">
     <FavoriteIcon />
-  </span>
+  </span> */}
   <span className="FeatureLabel">{t("homeScreen.save")}</span>
 </div>
 
 <div className="HistorySection FeatureRow">
-  <span className="FeaterIcon">
+  {/* <span className="FeaterIcon">
     <HistoryIcon />
-  </span>
+  </span> */}
   <span className="FeatureLabel">{t("homeScreen.history")}</span>
 </div>
-
+    </div>
 <Button
   className="FreeWithLoginBtn"
   onClick={() => navigate("/login")}
@@ -55,7 +50,7 @@ export function FeatureHint() {
   {isHebrew ? <ArrowBackIosIcon /> : <ArrowForwardIosIcon />}
 </Button>
           
-        </div>
+    </div>
       )}
     </div>
   );
