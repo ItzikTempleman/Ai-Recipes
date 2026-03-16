20a894f9801d12e423500c6f8011a77de8b33493
itzik templeman <itzik.templeman@gmail.com>
2026-03-16 16:34:01 +0200

    f

--- Frontend/src/Components/UserComponents/AdminScreen/AdminScreen.tsx
+++ Frontend/src/Components/UserComponents/AdminScreen/AdminScreen.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import "./AdminScreen.css";
+import { adminService } from "../../../Services/AdminService";
 
+
+type Statistics = {
+    usersEnteredSite: number,
+    guestsEnteredSite: number,
+    usersWhoGeneratedRecipes: number,
+    guestsWhoGeneratedRecipes: number,
+    totalRecipesGenerated: number
+}
 
 export function AdminScreen() {
+const adminStats=  adminService.getStatistics()
+
+  const [data, setData] = useState<Statistics[]>([]);
 
 
 
